11a5430aa7468ae801cab57e9c689961754fbe8f
juan alex <juan_AVC789@hotmail.com>
2024-12-30 18:27:51 -0500

    30-12-24

--- app/Models/config.php
+++ app/Models/config.php
@@ -3,12 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model; 
 
 class config extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $table = "configs";
     protected $primaryKey = 'config_id';
     public $timestamps = true;
